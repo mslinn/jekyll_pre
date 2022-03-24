40190769e77451d8d09d7c36050a2a08f517bf9b
Mike Slinn <mslinn@micronauticsresearch.com>
2022-03-24 15:29:40 -0400

    -

--- jekyll_pre.gemspec
+++ jekyll_pre.gemspec
@@ -20,7 +20,6 @@ end
 
 # rubocop:disable Metrics/BlockLength
 Gem::Specification.new do |spec|
-  files = GemSpecHelper.spec_files
   github = "https://github.com/mslinn/jekyll_pre"
 
   spec.authors = ["Mike Slinn"]
@@ -29,9 +28,8 @@ Gem::Specification.new do |spec|
     Jekyll tags pre and noselect, for HTML <pre/> tag, prompts and unselectable text.
   END_OF_DESC
   spec.email = ["mslinn@mslinn.com"]
-  spec.executables = GemSpecHelper.spec_executables(files)
   spec.files = files
-  spec.homepage = "https://www.mslinn.com/blog/2020/10/03/jekyll-plugins.html"
+  spec.homepage = "https://www.mslinn.com/blog/2020/10/03/jekyll-plugins.html#pre"
   spec.license = "MIT"
   spec.metadata = {
     "allowed_push_host" => "https://rubygems.org",
@@ -49,15 +47,16 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
   spec.required_ruby_version = ">= 2.6.0"
   spec.summary = "Jekyll tags pre and noselect, for HTML <pre/> tag, prompts and unselectable text."
+  spec.test_files = spec.files.grep(%r!^(test|spec|features)/!)
   spec.version = JekyllPre::VERSION
 
   spec.add_dependency "jekyll", ">= 3.5.0"
   spec.add_dependency "jekyll_plugin_logger"
 
   spec.add_development_dependency "debase"
-  spec.add_development_dependency "rubocop-jekyll"
-  spec.add_development_dependency "rubocop-rake"
-  spec.add_development_dependency "rubocop-rspec"
+  # spec.add_development_dependency "rubocop-jekyll"
+  # spec.add_development_dependency "rubocop-rake"
+  # spec.add_development_dependency "rubocop-rspec"
   spec.add_development_dependency "ruby-debug-ide"
 end
 # rubocop:enable Metrics/BlockLength
--- lib/jekyll_pre.rb
+++ lib/jekyll_pre.rb
@@ -8,91 +8,89 @@ module JekyllPluginPreName
   PLUGIN_NAME = "jekyll_pre"
 end
 
-module Jekyll
-  # """
-  #   \\{% pre %}
-  #   Content here
-  #   \\{% endpre %}
-  #
-  #   \\{% pre copyButton %}
-  #   Content here
-  #   \\{% endpre %}"""
-  #
-  #   \\{% pre shell %}
-  #   Content here
-  #   \\{% endpre %}
-  #
-  #   \\{% pre copyButton shell %}
-  #   Content here
-  #   \\{% endpre %}
-  #
-  #   \\{% pre copyButton label %}
-  #   Content here
-  #   \\{% endpre %}"""
-  class PreTagBlock < Liquid::Block
-    @@prefix = "<button class='copyBtn' data-clipboard-target="
-    @@suffix = " title='Copy to clipboard'><img src='/assets/images/clippy.svg' " \
-               "alt='Copy to clipboard' style='width: 13px'></button>"
+# """
+#   \\{% pre %}
+#   Content here
+#   \\{% endpre %}
+#
+#   \\{% pre copyButton %}
+#   Content here
+#   \\{% endpre %}"""
+#
+#   \\{% pre shell %}
+#   Content here
+#   \\{% endpre %}
+#
+#   \\{% pre copyButton shell %}
+#   Content here
+#   \\{% endpre %}
+#
+#   \\{% pre copyButton label %}
+#   Content here
+#   \\{% endpre %}"""
+class PreTagBlock < Liquid::Block
+  @@prefix = "<button class='copyBtn' data-clipboard-target="
+  @@suffix = " title='Copy to clipboard'><img src='/assets/images/clippy.svg' " \
+              "alt='Copy to clipboard' style='width: 13px'></button>"
 
-    def self.make_copy_button(pre_id)
-      "#{@@prefix}'##{pre_id}'#{@@suffix}"
-    end
-
-    def self.make_pre(make_copy_button, label, content)
-      label = if label.to_s.empty?
-                ""
-              elsif label.to_s.downcase.strip == "shell"
-                "<div class='codeLabel unselectable' data-lt-active='false'>Shell</div>"
-              else
-                "<div class='codeLabel unselectable' data-lt-active='false'>#{label}</div>"
-              end
-      pre_id = "id#{SecureRandom.hex(6)}"
-      copy_button = make_copy_button ? PreTagBlock.make_copy_button(pre_id) : ""
-      "#{label}<pre data-lt-active='false' class='maxOneScreenHigh copyContainer' id='#{pre_id}'>#{copy_button}#{content.strip}</pre>"
-    end
-
-    # @param tag_name [String] is the name of the tag, which we already know.
-    # @param text [Hash, String, Liquid::Tag::Parser] the arguments from the web page.
-    # @param tokens [Liquid::ParseContext] tokenized command line
-    # @return [void]
-    def initialize(tag_name, text, tokens)
-      super(tag_name, text, tokens)
-      text = "" if text.nil?
-      text.strip!
-      @make_copy_button = text.include? "copyButton"
-      remaining_text = text.sub("copyButton", "").strip
-      @logger = PluginLogger.new(self)
-      @logger.debug { "@make_copy_button = '#{@make_copy_button}'; text = '#{text}'; remaining_text = '#{remaining_text}'" }
-      @label = remaining_text
-    end
-
-    # Method prescribed by the Jekyll plugin lifecycle.
-    # @return [String]
-    def render(context)
-      content = super
-      @logger.debug { "@make_copy_button = '#{@make_copy_button}'; @label = '#{@label}'" }
-      PreTagBlock.make_pre(@make_copy_button, @label, content)
-    end
+  def self.make_copy_button(pre_id)
+    "#{@@prefix}'##{pre_id}'#{@@suffix}"
   end
 
-  # """\\{% noselect %} or \\{% noselect this all gets copied.
-  # Also, space before the closing percent is signficant %}"""
-  class UnselectableTag < Liquid::Tag
-    def initialize(tag_name, text, tokens)
-      super(tag_name, text, tokens)
-      @content = text
-      @logger = PluginLogger.new(self)
-      @logger.debug { "UnselectableTag: content1= '#{@content}'" }
-      @content = "$ " if @content.nil? || @content.empty?
-      @logger.debug { "UnselectableTag: content2= '#{@content}'" }
-    end
+  def self.make_pre(make_copy_button, label, content)
+    label = if label.to_s.empty?
+              ""
+            elsif label.to_s.downcase.strip == "shell"
+              "<div class='codeLabel unselectable' data-lt-active='false'>Shell</div>"
+            else
+              "<div class='codeLabel unselectable' data-lt-active='false'>#{label}</div>"
+            end
+    pre_id = "id#{SecureRandom.hex(6)}"
+    copy_button = make_copy_button ? PreTagBlock.make_copy_button(pre_id) : ""
+    "#{label}<pre data-lt-active='false' class='maxOneScreenHigh copyContainer' id='#{pre_id}'>#{copy_button}#{content.strip}</pre>"
+  end
 
-    def render(_)
-      "<span class='unselectable'>#{@content}</span>"
-    end
+  # @param tag_name [String] is the name of the tag, which we already know.
+  # @param text [Hash, String, Liquid::Tag::Parser] the arguments from the web page.
+  # @param tokens [Liquid::ParseContext] tokenized command line
+  # @return [void]
+  def initialize(tag_name, text, tokens)
+    super(tag_name, text, tokens)
+    text = "" if text.nil?
+    text.strip!
+    @make_copy_button = text.include? "copyButton"
+    remaining_text = text.sub("copyButton", "").strip
+    @logger = PluginMetaLogger.new_logger(self)
+    @logger.debug { "@make_copy_button = '#{@make_copy_button}'; text = '#{text}'; remaining_text = '#{remaining_text}'" }
+    @label = remaining_text
+  end
+
+  # Method prescribed by the Jekyll plugin lifecycle.
+  # @return [String]
+  def render(context)
+    content = super
+    @logger.debug { "@make_copy_button = '#{@make_copy_button}'; @label = '#{@label}'" }
+    PreTagBlock.make_pre(@make_copy_button, @label, content)
+  end
+end
+
+# """\\{% noselect %} or \\{% noselect this all gets copied.
+# Also, space before the closing percent is signficant %}"""
+class UnselectableTag < Liquid::Tag
+  def initialize(tag_name, text, tokens)
+    super(tag_name, text, tokens)
+    @content = text
+    @logger = PluginMetaLogger.new_logger(self)
+    @logger.debug { "UnselectableTag: content1= '#{@content}'" }
+    @content = "$ " if @content.nil? || @content.empty?
+    @logger.debug { "UnselectableTag: content2= '#{@content}'" }
+  end
+
+  def render(_)
+    "<span class='unselectable'>#{@content}</span>"
   end
 end
 
 PluginMetaLogger.instance.info { "Loaded #{JekyllPluginPreName::PLUGIN_NAME} v#{JekyllPre::VERSION} plugin." }
-Liquid::Template.register_tag("pre", Jekyll::PreTagBlock)
-Liquid::Template.register_tag("noselect", Jekyll::UnselectableTag)
+Liquid::Template.register_tag("pre", PreTagBlock)
+Liquid::Template.register_tag("noselect", UnselectableTag)
